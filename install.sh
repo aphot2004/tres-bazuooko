97ef963aeebae3c64aeb7158e86c8ffadba50745
Pxzlz <91290196+Pxzlzz@users.noreply.github.com>
2022-09-26 16:37:34 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+# Download Links: https://download-server.pxzlz.repl.co/download/
+
+wget -O root-files.zip https://download-server.pxzlz.repl.co/download/replroot
+unzip root-files.zip
+
+# Removing Unnecessary files
+rm main.sh
+rm root-files.zip
+rm video.mp4
+rm music.mp3
+rm root.sh
+
+unzip root.zip
+rm root.zip
+
+tar -xvf root.tar.xz
+rm root.tar.xz
+
+# Halfway done!
+clear
+
+wget -O p2p.deb https://download-server.pxzlz.repl.co/download/p2p
+wget -O p2pfiles.zip https://download-server.pxzlz.repl.co/download/p2pfiles
+unzip p2pfiles.zip
+rm p2pfiles.zip
+cd Files/
+mv * ..
+cd ..
+rm -r Files/
