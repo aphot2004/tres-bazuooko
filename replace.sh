59d11dee9c510697c76b271bfb4740bbadf0a283
Pxzlz <91290196+Pxzlzz@users.noreply.github.com>
2022-09-26 17:00:27 -0400

    Create replace.sh

---
+++ replace.sh
@@ -0,0 +1,4 @@
+rm replit.nix
+rm .replit
+wget -O .replit https://raw.githubusercontent.com/Pxzlzz/Peer2Profit-Replit/main/.replit
+wget -O replit.nix https://raw.githubusercontent.com/Pxzlzz/Peer2Profit-Replit/main/replit.nix
